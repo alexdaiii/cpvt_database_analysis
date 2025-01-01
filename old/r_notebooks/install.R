cdbc7cb6d5adcdf994c8c0285090b944a2b009f2
Alex Chang <25103853+alexdaiii@users.noreply.github.com>
2025-01-01 17:09:47 -0500

    cleanup old notebooks

--- old/r_notebooks/install.R
+++
@@ -1,9 +0,0 @@
-# Install required R packages
-
-requirements <- c("rstatix", "effectsize", "rcompanion")
-
-for (package in requirements) {
-  if (!require(package, character.only = TRUE)) {
-    install.packages(package)
-  }
-}
